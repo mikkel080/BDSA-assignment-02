8fcc02bbd3892740ac17aeed963b0782efe320eb
Magnus Brun Larsen <magnusbrunlarsen00@gmail.com>
2022-09-20 08:30:30 +0200

    Add extension methods and tests

--- Assignment2.Tests/ExtensionsTests.cs
+++ Assignment2.Tests/ExtensionsTests.cs
@@ -3,8 +3,38 @@ namespace Assignment2.Tests;
 public class ExtensionsTests
 {
     [Fact]
-    public void Test1()
+    public void IsSecure_given_uri_returns_true()
     {
+        //Arrange
+        var uri = new Uri("https://learnit.itu.dk/my/");
 
+        //Act
+        var result = uri.IsSecure();
+
+        //Assert
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public void IsSecure_given_uri_returns_false()
+    {
+        //Arrange
+        var uri = new Uri("http://www.google.com/");
+
+        //Act
+        var result = uri.IsSecure();
+
+        //Assert
+        result.Should().BeFalse();
+    }
+
+    [Fact]
+    public void WordCount_given_string_returns_7()
+    {
+        var toTest = "this is ,  a. string:; ; :; + which whould return 7";
+
+        var result = toTest.WordCount();
+
+        result.Should().Be(7);
     }
 }
--- Assignment2/Extensions.cs
+++ Assignment2/Extensions.cs
@@ -1,5 +1,12 @@
 namespace Assignment2;
 
+using System.Text.RegularExpressions;
+
 public static class Extensions
 {
+    public static bool IsSecure(this Uri uri) => 
+        uri.Scheme == "https"? true: false;
+
+    public static int WordCount(this string stream) => 
+        Regex.Matches(stream, @"\p{L}+").Count;
 }
